85da9a578e0eb87dceadf95959116ce36900899b
Rajat Vig <rajat.vig@gmail.com>
2016-01-20 13:15:43 -0800

    reset horizon with direnv and copy it back

--- functions/horizon_reset_env.fish
+++ functions/horizon_reset_env.fish
@@ -1,6 +1,9 @@
 function horizon_reset_env
   mv openstack_dashboard/local/local_settings.py /tmp
+  mv .envrc /tmp
   git clean -dfx
+  mv /tmp/.envrc .
+  mv /tmp/local_settings.py .
   cd ~
   cd -
   npm install
